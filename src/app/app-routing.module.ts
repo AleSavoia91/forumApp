4824eac05b4134dcf5209aeac582a336acc89652
AleSavoia91 <alesavoia91@gmail.com>
2021-05-07 23:41:51 +0200

    Add contactsModule and contactsComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LayoutComponent} from './core/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LayoutComponent, children: [
+      {path: 'home',
+      loadChildren: () => import('./features/home/home.module')
+        .then(m => m.HomeModule)},
+      {path: 'login',
+      loadChildren: () => import('./features/login/login.module')
+        .then(m => m.LoginModule)
+      },
+      {path: 'search',
+        loadChildren: () => import('./features/search/search.module')
+          .then(m => m.SearchModule)
+      },
+      {path: 'signup',
+        loadChildren: () => import('./features/signup/signup.module')
+          .then(m => m.SignupModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LayoutComponent} from './core/layout/layout.component';
+import {LayoutModule} from './core/layout/layout.module';
+import { LoginComponent } from './features/login/login.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
